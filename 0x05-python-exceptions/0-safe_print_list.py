67b7751c380d4946a3b395c95b175752c0197480
yvonnewakamau <yvonnefaith58@gmail.com>
2022-06-20 10:17:38 -0700

    goodness

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,9 @@ def safe_print_list(my_list=[], x=0):
     for idx in range(x):
         try:
             print(f'{my_list[idx]}', end='')
+
         except (IndexError):
-        continue
+            continue
+
         counter += 1                                                                print()
     return (counter)
