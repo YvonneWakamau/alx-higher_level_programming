d658fa341ccf55c812417f30e59b7ec78768a195
yvonnewakamau <yvonnefaith58@gmail.com>
2022-06-20 10:52:54 -0700

    content

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    counter = 0
-    for idx in range(x):
+    count = 0
+    for i in range(0, x):
         try:
-            print(f'{my_list[idx]}', end='')
-
-        except (IndexError):
-            continue
-
-        counter += 1                                                                print()
-    return (counter)
+            print("{:d}".format(my_list[i]), end="")                                        count += 1
+        except:
+            pass
+    print()                                                                         return count
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    counter = 0
-    for idx in range(x):
+    count = 0
+    for i in range(x):
         try:
-            print("{:d}".format(my_list[idx]), end='')                                  except (ValueError, TypeError):
-            continue
-        counter += 1                                                                print()
+            print("{:d}".format(my_list[i]), end="")                                 
+            count += 1
+        except (ValueError, TypeError):
+            pass
+    print()
     return (counter)
